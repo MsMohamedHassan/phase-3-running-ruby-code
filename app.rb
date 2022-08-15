09f3ec3f0072001685701dc75157bd31b854baf6
Zamzam <Zamzamhassan44@gmail.com>
2022-08-15 23:32:15 +0300

    running ruby code

---
+++ app.rb
@@ -0,0 +1,15 @@
+puts "Hello World!"
+
+# # print "Hello world!"
+# # print "Hello world!"
+# # print "Hello world!"
+
+# puts [1,2,3].inspect
+
+# # Inspecting Data with p and pp
+
+ p [1,2,3]
+
+pp [{id:1, hello: "World"}, {id:2, hello: "Ruby"}, {id:3, hello: "Moon"}, {id:4, hello: "Learner"}]
+
+print "Pass this test, please." 
