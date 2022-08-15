6fca94cb6b89e4ebf49f06ba6ace701a82c6a37d
Zamzam <Zamzamhassan44@gmail.com>
2022-08-16 00:17:06 +0300

    running ruby code

--- app.rb
+++ app.rb
@@ -1,5 +1,8 @@
 puts "Hello World!"
 
+print "Pass this test, please." 
+
+
 # # print "Hello world!"
 # # print "Hello world!"
 # # print "Hello world!"
@@ -10,6 +13,5 @@ puts "Hello World!"
 
  p [1,2,3]
 
-pp [{id:1, hello: "World"}, {id:2, hello: "Ruby"}, {id:3, hello: "Moon"}, {id:4, hello: "Learner"}]
+# pp [{id:1, hello: "World"}, {id:2, hello: "Ruby"}, {id:3, hello: "Moon"}, {id:4, hello: "Learner"}]
 
-print "Pass this test, please." 
